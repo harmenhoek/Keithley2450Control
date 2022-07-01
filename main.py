5aa4be70b04bb51b0f48ab13b6445b87596ec2f3
harmenhoek <62509463+harmenhoek@users.noreply.github.com>
2022-07-01 11:50:52 +0200

    pyinstaller import pymeasure bugfix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from pymeasure.instruments.keithley import Keithley2450
+import pymeasure  # need to import whole library, otherwise pyinstaller fails
+# from pymeasure.instruments.keithley import Keithley2450
 from datetime import datetime
 import time
 from mariobeep import mariobeep
+
 
 readout_rate = 1.0  # frequency in hz
 
@@ -12,7 +14,7 @@ print(rm.list_resources())
 
 try:
     # keithley = Keithley2450('ASRL3::INSTR')
-    keithley = Keithley2450('USB0::0x05E6::0x2450::04456958::INSTR')
+    keithley = pymeasure.instruments.keithley.Keithley2450('USB0::0x05E6::0x2450::04456958::INSTR')
     keithley.measure_current()
     keithley.measure_voltage()
     mariobeep(keithley)
@@ -46,6 +48,6 @@ target_voltage = 3
 steps = 10
 pause = 1
 keithley.ramp_to_voltage(target_voltage, steps, pause)
-
+  
             
 '''
