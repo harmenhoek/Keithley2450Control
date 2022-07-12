5c9e554fa0f4b4662e0326c4905dcbe9ac83d7e4
harmenhoek <62509463+harmenhoek@users.noreply.github.com>
2022-07-12 16:39:34 +0200

    Buffer and bugfixes

--- main.py
+++ main.py
@@ -13,31 +13,38 @@ steps = 10  # int number of steps
 pause = 3  # pause in seconds
 
 import pyvisa
+
 rm = pyvisa.ResourceManager()  # should use Keysight by default
 print(rm.list_resources())
 
-
 try:
-    # keithley = Keithley2450('ASRL3::INSTR')
-    # keithley = pymeasure.instruments.keithley.Keithley2450('USB0::0x05E6::0x2450::04456958::INSTR')
     keithley = Keithley2450('USB0::0x05E6::0x2450::04456958::INSTR')
-    keithley.measure_current()
-    keithley.measure_voltage()
-    keithley.enable_source()  # Enables the source output
     # mariobeep(keithley)
+
+    keithley.reset()
+    keithley.use_front_terminals()
+    keithley.measure_current()
+    keithley.enable_source()
+
+    # keithley.apply_voltage()
+
+    keithley.config_buffer(points=100)
+    keithley.start_buffer()
 except:
     raise ConnectionError("Unable to connect to the Keithley 2450.")
 
 
 def ReadOutToLogFile(f, setvoltage):
     datetimestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    print(f"{datetimestamp}, {keithley.current}, {keithley.voltage}, {setvoltage}")
-    f.write(f"{datetimestamp}, {keithley.current}, {keithley.voltage}, {setvoltage}\n")
+    print(f"{datetimestamp}, {keithley.mean_current}, {keithley.mean_voltage}, {setvoltage}")
+    f.write(f"{datetimestamp}, {keithley.mean_current}, {keithley.mean_voltage}, {setvoltage}\n")
+    keithley.reset_buffer()
+    keithley.start_buffer()
 
 
-
-voltages = iter(np.linspace(0, target_voltage, steps))
+# voltages = iter(np.linspace(0, target_voltage, steps))
 setvoltage = None
+voltages = np.linspace(0, target_voltage, steps)
 
 datetimestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 filename = f"Keithley_Logfile_{datetimestamp}.csv"
@@ -47,7 +54,9 @@ with open(filename, 'w') as f:
     t_start = time.time()
     t_start_2 = time.time()
     ramping = False
-    while True:
+    loop = True
+    cntr = 0
+    while loop:
 
         # Log
         if time.time() - t_start > readout_rate:
@@ -58,20 +67,27 @@ with open(filename, 'w') as f:
             t_start = time.time()
 
         # Ramp voltage on enter press
-        if keyboard.read_event() == "enter":
+        if ramping is False and keyboard.is_pressed("a"):
             ramping = True
-            t_start_2 = time.time()
+            print(f'Ramping started with voltages: {voltages}.')
 
         if ramping and (time.time() - t_start_2 > pause):
-            setvoltage = next(voltages, None)
-            if setvoltage:
-                keithley.apply_voltage(setvoltage)
+            if cntr < len(voltages):
+                setvoltage = voltages[cntr]
+                # setvoltage = next(voltages, None)
+                print(f'Voltage set to {setvoltage}V.')
+                keithley.source_voltage = setvoltage
+                keithley.current
                 t_start_2 = time.time()
+                cntr = cntr + 1
             else:
                 print('Voltage ramping ended')
                 ramping = False
 
-
+        if keyboard.is_pressed("esc"):
+            print('Stopping now!')
+            keithley.shutdown()
+            loop = False
 
 '''
 keithley.enable_source()                # Enables the source output
@@ -79,6 +95,6 @@ target_voltage = 3
 steps = 10
 pause = 1
 keithley.ramp_to_voltage(target_voltage, steps, pause)
-  
-            
+
+
 '''
